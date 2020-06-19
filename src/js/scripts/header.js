d2ba9161a0a95cbd3602421c093cd747063a4c2a
DSGlem <davidsporysh@gmail.com>
2020-06-19 16:46:45 +0300

    style fixes, smooth scroll

--- src/js/scripts/header.js
+++ src/js/scripts/header.js
@@ -1,3 +1,11 @@
 $("#menu-switch").click(function () {
   $(this).parent().toggleClass("menu-opened");
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
