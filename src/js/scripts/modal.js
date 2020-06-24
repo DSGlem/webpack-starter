00043a929a89afc2a47c012e8f15fb9ad81ebc43
DSGlem <davidsporysh@gmail.com>
2020-06-24 17:19:41 +0300

    edits

--- src/js/scripts/modal.js
+++ src/js/scripts/modal.js
@@ -1,10 +1,15 @@
 let closeBtn = document.querySelectorAll(".btn-close");
 
 for (item of closeBtn) {
-    item.addEventListener("click", function () {
-
-        const layout = this.parentNode.parentNode;
-        console.log(layout);
-        layout.classList.remove("visible");
-    })
+  item.addEventListener("click", function () {
+    const layout = this.parentNode.parentNode;
+    console.log(layout);
+    layout.classList.remove("visible");
+  });
 }
+
+document.addEventListener("click", function (e) {
+  console.log($(e.currentTarget).find(".video"));
+  const src = $(e.currentTarget).find(".video").attr("src");
+  $(e.currentTarget).find(".video").attr("src", src);
+});
